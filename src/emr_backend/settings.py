a4caac5500851c6f2b88594c8517f68e3a7b7300
omiebi <omiebiiworima@gmail.com>
2025-07-20 03:24:42 +0100

    update src/emr_backend/settings.py for render deployment

--- src/emr_backend/settings.py
+++ src/emr_backend/settings.py
@@ -90,16 +90,34 @@ WSGI_APPLICATION = 'emr_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# This block should be added or modified to use DATABASE_URL
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True # For Render's PostgreSQL, this is often necessary
+    )
+else:
+    # Fallback for local development if DATABASE_URL isn't set
+    # This will use SQLite for local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
+
+#DATABASES = {
+#    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
+#}
 
 
 
